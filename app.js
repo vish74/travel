19591de1ac0f127fa477f19c392999a96ec7fd2a
Vishvesh Shah <vmshah@scu.edu>
2015-09-26 14:56:49 -0700

    working

--- app.js
+++ app.js
@@ -1,62 +1,69 @@
-var cfenv = require('cfenv');
+/*jshint node:true*/
+//------------------------------------------------------------------------------
+// Travel node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+// This application uses express as it's web server
+// for more info, see: http://expressjs.com
 var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var session = require('express-session');
+var routes = require('./routes/');
 var app = express();
 
+// get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(session({ name:'Alchemy', secret: 'abcd', resave: false, saveUninitialized: true }));
 
-app.use('/', routes);
-app.use('/users', users);
+//Basic Routing
+app.get('/', routes.index);
 
+
+//Change error handler
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message, error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message, error: {}
+    });
 });
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
