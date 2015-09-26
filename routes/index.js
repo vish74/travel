074ee30e8eb4dfd024fbb7b00e53e493f178a07a
Vishvesh Shah <vmshah@scu.edu>
2015-09-26 16:35:54 -0700

    remove

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var request = require('request');
 exports.index = function (req, res) {
  var id = req.params.id;
  //id = "6dcc4abc-00a8-4f0c-8eca-a467f29ca6c2";
-    console.dir(id);
  var get_url = "https://api-sandbox.traitify.com/v1/assessments/"+id+"data=types,traits&authorization=17nvgtl8vtvja296a1ctdthqk0";
  request.get({
         headers:    {'content-type' : 'text/json'},
