75ea90731664450e87a8ba4e8aa3097848cfd71e
Vishvesh Shah <vmshah@scu.edu>
2015-09-26 16:45:26 -0700

    minor

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var request = require('request');
 exports.index = function (req, res) {
  var id = req.params.id;
  //id = "6dcc4abc-00a8-4f0c-8eca-a467f29ca6c2";
- var get_url = "https://api-sandbox.traitify.com/v1/assessments/"+id+"data=types,traits&authorization=17nvgtl8vtvja296a1ctdthqk0";
+ var get_url = "https://api-sandbox.traitify.com/v1/assessments/"+id+"/data=types,traits&authorization=17nvgtl8vtvja296a1ctdthqk0";
  request.get({
         headers:    {'content-type' : 'text/json'},
         url:        get_url
