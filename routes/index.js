d873e05a3ccc8d1e0db44e00ab1af02e735b7aea
Vishvesh Shah <vmshah@scu.edu>
2015-09-26 17:06:25 -0700

    added function

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,21 @@ exports.index = function (req, res) {
  var id = req.params.id;
  //id = "6dcc4abc-00a8-4f0c-8eca-a467f29ca6c2";
  var get_url = "https://api-sandbox.traitify.com/v1/assessments/"+id+"?data=types,traits&authorization=17nvgtl8vtvja296a1ctdthqk0";
- request.get({
-        headers:    {'content-type' : 'text/json'},
-        url:        get_url
-    }, function(error, response, body){
-     console.dir(body);
-    });
+
+
+    Getfrom(get_url, function(callback){
+
+        console.log(callback);
+    })
 
 };
+
+
+function Getfrom(url, callback) {
+    request.get({
+        headers:    {'content-type' : 'text/json'},
+        url:        url
+    }, function(error, response, body){
+        callback(body);
+    });
+}
