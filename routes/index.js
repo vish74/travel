ac8a88e4aaa632677c4b16a89b0406dc1cccd77c
Vishvesh Shah <vmshah@scu.edu>
2015-09-26 17:47:55 -0700

    added blend

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 var request = require('request');
 exports.index = function (req, res) {
  var id = req.params.id;
- //id = "6dcc4abc-00a8-4f0c-8eca-a467f29ca6c2";
- var get_url = "https://api-sandbox.traitify.com/v1/assessments/"+id+"?data=types,traits&authorization=17nvgtl8vtvja296a1ctdthqk0";
+ var get_url = "https://api-sandbox.traitify.com/v1/assessments/"+id+"?data=blend,types,traits&authorization=17nvgtl8vtvja296a1ctdthqk0";
 
 
     Getfrom(get_url, function(callback){
